bbfe9f050a5c86bf41719cdf959a5684899d7871
ivanmijacika <imijacika20@stuy.edu>
2020-11-21 16:05:07 -0500

    equals added

--- RealNumber.java
+++ RealNumber.java
@@ -19,7 +19,13 @@ public class RealNumber{
   *Special case: if one is exactly zero, the other must be exactly zero.
   */
   public boolean equals(RealNumber other){
-    return true;
+    if (getValue()==0){
+      if (other.getValue()==0) return true;
+      else return false;
+    }
+    else{
+      return (Math.abs(getValue() - other.getValue()) / getValue()) < 0.00001;
+    }
   }
 
   /*
