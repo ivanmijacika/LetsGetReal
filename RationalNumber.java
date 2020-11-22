8670e59e22a2b549d943d285b615bae955c79668
ivanmijacika <imijacika20@stuy.edu>
2020-11-21 21:09:53 -0500

    divide added

--- RationalNumber.java
+++ RationalNumber.java
@@ -110,7 +110,8 @@ public class RationalNumber extends RealNumber{
   *Return a new RationalNumber that is the this divided by the other
   */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    RationalNumber quotient = new RationalNumber(getNumerator() * other.getDenominator(), getDenominator() * other.getNumerator());
+    return quotient;
   }
 
   /**
