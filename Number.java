17c6f1bd821b994f32b37d44cc6990b813a13ec8
ivanmijacika <imijacika20@stuy.edu>
2020-11-24 18:46:38 -0500

    compareTo added

--- Number.java
+++ Number.java
@@ -1,7 +1,21 @@
 public abstract class Number{
-
   public abstract double getValue();
 
+  /*return 0 when this Number equals the other RealNumber
+  return a negative value when this Number is smaller than the other Number
+  return a positive value when this Number is larger than the other Number
+  */
+  public int compareTo(Number other){
+    if (equals(other)) return 0;
+    else if (getValue() > other.getValue()) return 1;
+    else return 1;
+  }
+
+  /*
+  *Return true when the % difference of the values
+  *are within 0.00001 of eachother.
+  *Special case: if one is exactly zero, the other must be exactly zero.
+  */
   public boolean equals(Number other){
     if (getValue()==0){
       if (other.getValue()==0) return true;
@@ -11,6 +25,4 @@ public abstract class Number{
       return (Math.abs(getValue() - other.getValue()) / getValue()) < 0.00001;
     }
   }
-
-
 }
